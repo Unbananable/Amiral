38f802e394e38b3434ed8c9daa6b62d713c87402
Anaelle LECLABART <anleclab@e3r8p19.42.fr>
2019-01-17 17:46:21 +0100

    fixed map error in reader.c and updated fdf.h

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: dtrigalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 15:55:35 by dtrigalo          #+#    #+#             */
-/*   Updated: 2019/01/17 16:47:49 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2019/01/17 17:27:00 by anleclab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 
 typedef struct	s_point
 {
-	int	x;
-	int	y;
+	int		x;
+	int		y;
 }				t_point;
 
+typedef struct	s_map
+{
+	int		width;
+	int		depth;
+	int		scale;
+}				t_map;
+
 void	draw_line(void *mlx_ptr, void *win_ptr, t_point p0, t_point p1);
+int		**reader(char *file_name);
 
 #endif
