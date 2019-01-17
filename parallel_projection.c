141512b5f784bfd22ebafb3d89e3dcabd9100e77
Anaelle LECLABART <anleclab@e3r8p19.42.fr>
2019-01-17 18:54:20 +0100

    basic parallel_projection

---
+++ parallel_projection.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parallel_projection.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anleclab <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/17 18:17:01 by anleclab          #+#    #+#             */
+/*   Updated: 2019/01/17 18:53:55 by anleclab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* TO DO
+ * - gestion des erreurs (fuites memoire etc.)
+ * - formules de calcul des coordonnees (cf cahier)
+*/
+
+#include <math.h>
+
+t_point	**parallel_projection(int **map, t_map *map_info)
+{
+	t_point	**res;
+	double	angle;
+	double	ratio;
+	t_point	i;
+
+	angle = 45.0;
+	ratio = 1.0;
+	if (!(res = (t_point **)malloc(sizeof(t_point *) * map_info->depth)))
+		error("MALLOC TOUT PETE");
+	i.y = -1;
+	while (++(i.y) < map_info->depth)
+	{
+		if (!(res[i.y] = (t_point *)malloc(sizeof(t_point) * map_info->width)))
+			error("SACRE MALLOC !");
+		i.x = -1;
+		while (++(i.x) < map_info->length)
+		{
+			if ((res[i.y][i.x].x = (int)nearbyint(/**/)) > map_info->x_max)
+				map_info->x_max = res[i.y][i.x].x;
+			if ((res[i.y][i.x].y = (int)nearbyint(/**/)) > map_info->y_max)
+				map_info->y_max = res[i.y][i.x].y;
+		}
+	}
+}
