b88ba9123e28713dc367ed6aeb4d6c8946109af2
Dimitri TRIGALOUX <dtrigalo@e3r7p21.42.fr>
2019-01-18 15:32:17 +0100

    options idea

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anleclab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 17:51:55 by anleclab          #+#    #+#             */
-/*   Updated: 2019/01/18 14:38:41 by anleclab         ###   ########.fr       */
+/*   Updated: 2019/01/18 15:20:29 by dtrigalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,19 @@ int		deal_key(int key, void *param)
 //touche (key) utilisée et s'en servir pour la relier à des actions définies
 	key +=0;
 	param +=0;
-	return (0);
+	ft_putstr("Key: ");
+	ft_putnbr(key);
+	ft_putchar('\n');
+	if (key == 53)
+		exit(1);
+/*	if (key >= 123 || key <= 126)
+		camera_arrow(key); //camera_top, camera_bot, camera_right, camera_left
+	if (key == 4 || key == 5 || key == 24 || key == 27 || key == 69 || key == 78)
+		camera_lense(key); //camera_zoom, camera_unzoom (+: 69 24 4) (-: 78 27 5)
+	if (key == ? -Make a choice-)
+		color_panel(key); //Several sets of colors, depending of the key ('1'(key 18) to '5'(key 23) for example)
+	
+*/	return (0);
 }
 
 void	usage(void)
@@ -33,7 +45,7 @@ void	usage(void)
 int		main(int ac, char **av)
 {
 
-	int		**map;
+/*	int		**map;
 	t_point **proj_map;
 	t_map	map_info;
 	t_win	win;
@@ -42,12 +54,16 @@ int		main(int ac, char **av)
 		usage();
 	win.mlx_ptr = mlx_init();
 	win.win_ptr = mlx_new_window(win.mlx_ptr, WIN_HEIGHT, WIN_WIDTH, "FdF");
+	printf("Début (apres initialisation)\n");
 	map = reader(av[1], &map_info);
+	printf("Apres reader\n");
 	proj_map = parallel_projection(map, &map_info);
+	printf("Apres parallel_projection\n");
 	draw_in_win(win, proj_map, map_info);
-	mlx_loop(win.mlx_ptr);
+	printf("Apres draw_in_win\n");
+	mlx_loop(win.mlx_ptr);*/
 
-/*	void	*mlx_ptr;
+	void	*mlx_ptr;
 	void	*win_ptr;
 	t_point p0;
 	t_point p1;
@@ -63,7 +79,6 @@ int		main(int ac, char **av)
 	mlx_pixel_put(mlx_ptr, win_ptr, 1000, 1000, 0xFFFFFF);
 	mlx_key_hook(win_ptr, deal_key, (void *)0);
 //	draw_line(mlx_ptr, win_ptr, p0, p1);
-	draw_projection(proj_map, map_info);
-	mlx_loop(mlx_ptr);*/
+	mlx_loop(mlx_ptr);
 	return (0);
 }
