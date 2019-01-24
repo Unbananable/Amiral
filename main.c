ee84843f8df3b9724eb43245eba5e6582bd25e46
Dimitri TRIGALOUX <dtrigalo@e3r7p21.42.fr>
2019-01-24 13:52:42 +0100

    ability to close with red cross, added key press/release features

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anleclab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 17:51:55 by anleclab          #+#    #+#             */
-/*   Updated: 2019/01/24 13:35:10 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2019/01/24 13:52:09 by dtrigalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,40 @@
 
 #include <stdio.h>
 
+int		red_cross_closing(void *param)
+{
+	(void)param;
+	exit(0);
+	return (0);
+}
+
 int		mouse_press(int button, int x, int y, void *param)
 {
-	ft_printf("Test press: (x,y): %d,%d\n", x, y);
+	ft_printf("Mouse press: %d (x,y): %d,%d\n", button, x, y);
 	return (0);
 }
 
 int		mouse_release(int button, int x, int y, void *param)
 {
-	ft_printf("Test release: (x,y): %d,%d\n", x, y);
+	ft_printf("Mouse release: %d (x,y): %d,%d\n", button, x, y);
 	return (0);
 }
 
-
-int		mouse_hook(int button, int x, int y, void *param)
+int		key_press(int key, void *param)
 {
-	param += 0;
-	ft_printf("Mouse button: %d, (x,y): %d,%d\n", button, x, y);
-	return (0);
-}
-
-int		key_hook(int key, void *param)
-{
-//Action qui se déroule à chaque input clavier (on peut par exemple print la
-//touche (key) utilisée et s'en servir pour la relier à des actions définies
-	param +=0;
-	ft_putstr("Key: ");
-	ft_putnbr(key);
-	ft_putchar('\n');
+	ft_printf("Key press: %d\n", key);
 	if (key == 53)
 		exit(1);
+	return (0);
+}
+
+int		key_release(int key, void *param)
+{
+	ft_printf("Key release: %d\n", key);
+	return (0);
+}
+
+/* ******************* BONUS IDEAS ****************************************** */
 /*	if (key >= 123 || key <= 126)
 		camera_arrow(key,...); //camera_top, camera_bot, camera_right, camera_left
 	if (key == 4 || key == 5 || key == 24 || key == 27 || key == 69 || key == 78)
@@ -59,9 +63,8 @@ int		key_hook(int key, void *param)
 	if (key == 35 ('P'))
 		switch_projection(); //Switch between the 2 projections (+ affichage graphique du nom de la projection ?)
 	if (key == 8 ('C'))
-		activation_colours(); //Activation/désactivation des couleurs (une touche pour activer les couleurs et une touche pour les desactiver ou sinon il faut un interrupteur (var static ?) pour mémoriser si les couleurs sont actuellement affichées ou non)
-*/	return (0);
-}
+		activation_colours(); //Activation/désactivation des couleurs (une touche pour activer les couleurs et une touche pour les desactiver ou sinon il faut un interrupteur (var static ?) pour mémoriser si les couleurs sont actuellement affichées ou non)*/
+/* ************************************************************************** */
 
 static void	init_map_info(t_map *map_info, char *file_name)
 {
@@ -91,10 +94,6 @@ void	usage(void)
 
 int		main(int ac, char **av)
 {
-	static int		click_and_draw[2][2];
-	t_point p1;
-	t_point p2;
-
 	int		**map;
 	t_point **proj_map;
 	t_map	map_info;
@@ -109,31 +108,14 @@ int		main(int ac, char **av)
 	proj_map = isometric_projection(map, &map_info);
 	get_placement_info(proj_map, &map_info);
 	draw_in_win(win, proj_map, map_info);
-	win.map_info = map_info;
-	mlx_key_hook(win.win_ptr, key_hook, (void *)0);
+	mlx_hook(win.win_ptr, 2, 0, key_press, (void *)0);
+	mlx_hook(win.win_ptr, 3, 0, key_release, (void *)0);
 //	mlx_mouse_hook(win.win_ptr, mouse_hook, (void *)0);
 /* On veut tracer une ligne avec les deux prochaines instructions             */
 	mlx_hook(win.win_ptr, 4, 0, mouse_press, (void *)0);
 	mlx_hook(win.win_ptr, 5, 0, mouse_release, (void *)0);
 /* ************************************************************************** */
+	mlx_hook(win.win_ptr, 17, 0, red_cross_closing, (void *)0);
 	mlx_loop(win.mlx_ptr);
-
-/*	void	*mlx_ptr;
-	void	*win_ptr;
-	t_point p0;
-	t_point p1;
-	ac += 0;
-	av += 0;
-// TO DO: fonction init_point(x ,y ,z) ?
-	p0.x = 1;
-	p0.y = 1;
-	p1.x = 100;
-	p1.y = 100;
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "Bonjour bonjour !!");
-	mlx_pixel_put(mlx_ptr, win_ptr, 1000, 1000, 0xFFFFFF);
-	mlx_key_hook(win_ptr, deal_key, (void *)0);
-//	draw_line(mlx_ptr, win_ptr, p0, p1);
-	mlx_loop(mlx_ptr);*/
 	return (0);
 }
